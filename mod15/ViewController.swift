7cc0a76cb07234ec4cd6e5f785fef6522b18aeb9
Fox <typefox@icloud.com>
2021-01-26 12:02:20 +0300

    Добавила доработки

--- mod15/ViewController.swift
+++ mod15/ViewController.swift
@@ -3,89 +3,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-//  Задание1:  pure function - «чистая функция», она не изменяет тип данных, которые были у нее на входе.Это делает код более лаконичным, с ней не нужно копипастить код, можно просто вызвать функцию.
-// Задание 9: infix - оператор который, работает с между двумя аргументами (необязательно с числами).
-    //• Infix: (x) + (y)
-    //• Prefix: + (x) (y)
-    //• Postfix: (x) (y) +
+//  Задание1:  Функция считается чистой, когда не производит никаких побочных эффектов и когда она не полагается на какое-либо внешнее состояние. Основная мысль заключается в том, что она всегда будет производить один и тот же результат для заданного набора входных данных - независимо от того, когда и сколько раз она вызывается.
+// Задание 9:
+//    - infix определять свою логику для бинарных операторов (+-)
+//    - prefix определить свою логику для унарного минуса (только один операнд)
+//    - postfix определить свою логику оператора, который следует за операндом (b!) (только один операнд)
     
-    @IBAction func clearButton(_ sender: Any) {
+    @IBAction func clearButton() {
         label.text = "\(numbers) + \(names)"
         label.backgroundColor = .white
         view.backgroundColor = .white
     }
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func task4(_ sender: Any) {
-        task4()
+    @IBAction func task4() {
+        task40()
     }
-    @IBAction func task3(_ sender: Any) {
-        task3()
+    @IBAction func task3() {
+        task30()
     }
-    @IBAction func task5(_ sender: Any) {
-        task5()
+    @IBAction func task5() {
+        task50()
     }
     
-    @IBAction func task6(_ sender: Any) {
-        task6 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: { self.label.text = "2 second later"})
-            }
+    @IBAction func task6() {
+        task60 { self.label.text = "2 second later" }
         }
         
-    @IBAction func task7(_ sender: Any) {
-        task7(f1: { self.view.backgroundColor = .red }, f2: { self.label.backgroundColor = .yellow })()
+    @IBAction func task7() {
+        task70(f1: { self.view.backgroundColor = .red }, f2: { self.label.backgroundColor = .yellow })()
     }
-    @IBAction func task8(_ sender: Any) {
-        let sortedArray = task8(array: numbers, by: partTask8)
+    @IBAction func task8() {
+        let sortedArray = task80(array: numbers, by: partTask8)
         label.text = "\(sortedArray)"
+
         }
     
     
     
-    let numbers: [Int] = [2, 4, 1, 5, 3]
+    let numbers: [Int] = [2, 5, 1, 5, 3]
     let names: [String] = ["Lena", "Masha", "Kate", "Nick"]
   
-    func task3() {
+    func task30() {
     let task3 = numbers.sorted(by: <)
         label.text = "\(task3)"
     }
-    func task4() {
+    func task40() {
         let task4 = numbers.map { String($0) }
         label.text = "\(task4)"
 
     }
-    func task5() {
+    func task50() {
         let task5 = names.reduce("") {
             names, name in name + " " + names
         }
         label.text = task5
     }
-    func task6(compleion: () -> Void) {
-        compleion()
-    }
+    func task60(execute: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: execute)
+}
     
-    func task7(f1: @escaping () -> Void, f2: @escaping () -> Void) -> () -> Void {
+    func task70(f1: @escaping () -> Void, f2: @escaping () -> Void) -> () -> Void {
         return {
             f1()
             f2()
         }
     }
-    func partTask8(x1: Int, x2: Int)->Bool{
-        let x1 = numbers[0]
-        let x2 = numbers[1]
-        return x2 > x1 ? true : false
+   func partTask8(x1: Int, x2: Int)->Bool{
+    return x2 > x1
     }
 
-    func task8(array: [Int], by: (Int, Int) -> Bool) -> [Int]{
+    func task80(array: [Int], by: (Int, Int) -> Bool) -> [Int]{
         return array.sorted(by: by)
     }
-   
+    
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "\(numbers) + \(names)"
-        label.backgroundColor = .white
-        view.backgroundColor = .white
+        
  
     }
     
