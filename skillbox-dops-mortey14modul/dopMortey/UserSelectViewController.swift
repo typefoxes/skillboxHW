bcba8be9f78ac87ccefb1a4802408bc779f1bf05
Fox <typefox@icloud.com>
2021-02-04 15:40:24 +0300

    Передала данные из ячейки во вью

--- skillbox-dops-mortey14modul/dopMortey/UserSelectViewController.swift
+++ skillbox-dops-mortey14modul/dopMortey/UserSelectViewController.swift
@@ -4,28 +4,21 @@ import UIKit
 import RealmSwift
 
 class UserSelectViewController: UIViewController {
-
+    
+    let realm = try! Realm()
+    
     var name = ""
     var gender = ""
     var location = ""
     var status = ""
     var species = ""
     
-    @IBAction func userName(_ sender: Any) {
-        
-    }
-    @IBAction func userGender(_ sender: Any) {
-    }
-    
-    @IBAction func userSpecies(_ sender: Any) {
-    }
-    @IBAction func userStatus(_ sender: Any) {
-    }
-    @IBAction func userLocation(_ sender: Any) {
-    }
-    @IBAction func saveButton(_ sender: Any) {
-    }
-
+    @IBAction func userName() { }
+    @IBAction func userGender() { }
+    @IBAction func userSpecies() { }
+    @IBAction func userStatus() { }
+    @IBAction func userLocation() { }
+    @IBAction func saveButton() { }
     
     @IBOutlet weak var nameU: UITextField!
     @IBOutlet weak var genderU: UITextField!
@@ -33,17 +26,13 @@ class UserSelectViewController: UIViewController {
     @IBOutlet weak var statusU: UITextField!
     @IBOutlet weak var speciesU: UITextField!
     
-    
-    
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        nameU.placeholder = name
-        genderU.placeholder = gender
-        locationU.placeholder = location
-        statusU.placeholder = status
-        speciesU.placeholder = species
+    super.viewDidLoad()
+        
+    nameU.placeholder = name
+    genderU.placeholder = gender
+    locationU.placeholder = location
+    statusU.placeholder = status
+    speciesU.placeholder = species
     }
-  
-
 }
